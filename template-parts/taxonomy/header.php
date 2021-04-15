412c19681be63b923c7795584b9ed317bfdf30bb
SebasTyler <ibikatti@gmail.com>
2021-04-15 17:11:28 -0400

    Done structured of Date view

--- template-parts/taxonomy/header.php
+++ template-parts/taxonomy/header.php
@@ -5,11 +5,18 @@
  * @package Mercurio
  * @subpackage Mercurio
  */
-
 ?>
 
 <div class="o-archiveTop">
     <h1 class="m-archiveTop__heading">
-        <span class="-colorLightin -extraBold"><?php _e( $args['name_taxonomy']->name ); ?></span>
+        <span class="-colorLightin -extraBold">
+        <?php
+        if (is_date( )) {
+            _e( single_month_title(' ', false ) );
+        } else {
+            _e( $args['name_taxonomy']->name );
+        }        
+        ?>
+        </span>
     </h1><!-- .m-archiveTop__heading -->
 </div><!-- .o-archiveTop -->
